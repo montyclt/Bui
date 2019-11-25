23ab6906b8490f9ed6fac80bf846b6b83bd88fc9
Ivan Montilla <montyclt@outlook.com>
2019-11-25 18:03:51 +0100

    Added dropdown, popover and button group.

--- MontyClt.Bui/Components/Popovers/Placement.cs
+++ MontyClt.Bui/Components/Popovers/Placement.cs
@@ -2,10 +2,25 @@
 {
     public sealed class Placement
     {
-        public readonly Placement Right = new Placement("right");
-        public readonly Placement Left = new Placement("left");
-        public readonly Placement Top = new Placement("top");
-        public readonly Placement Bottom = new Placement("bottom");
+        public static readonly Placement Right = new Placement("right");
+        public static readonly Placement RightStart = new Placement("right-start");
+        public static readonly Placement RightEnd = new Placement("right-end");
+
+        public static readonly Placement Left = new Placement("left");
+        public static readonly Placement LeftStart = new Placement("left-start");
+        public static readonly Placement LeftEnd = new Placement("left-end");
+
+        public static readonly Placement Top = new Placement("top");
+        public static readonly Placement TopStart = new Placement("top-start");
+        public static readonly Placement TopEnd = new Placement("top-end");
+
+        public static readonly Placement Bottom = new Placement("bottom");
+        public static readonly Placement BottomStart = new Placement("bottom-start");
+        public static readonly Placement BottomEnd = new Placement("bottom-end");
+
+        public static readonly Placement Auto = new Placement("auto");
+        public static readonly Placement AutoStart = new Placement("auto-start");
+        public static readonly Placement AutoEnd = new Placement("auto-end");
 
         private readonly string _value;
 
