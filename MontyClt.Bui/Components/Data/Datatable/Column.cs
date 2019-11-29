59f7625e258aa2373607a9c36b3412a3035b58ca
Ivan Montilla <montyclt@outlook.com>
2019-11-29 18:27:14 +0100

    Datatable

---
+++ MontyClt.Bui/Components/Data/Datatable/Column.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MontyClt.Bui.Components.Data.Datatable
+{
+    public class Column<T>
+    {
+        public string Header { get; set; }
+        public bool Sortable { get; set; }
+        public Func<T, object> Content { get; set; }
+
+        public Column(string header, Func<T, object> content, bool sortable = false)
+        {
+            Header = header;
+            Content = content;
+            Sortable = sortable;
+        }
+    }
+}
