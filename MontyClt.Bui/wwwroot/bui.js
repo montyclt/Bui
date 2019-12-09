3901263b240db60c2e2037f7dfe1f980e4cf9c1b
Ivan Montilla <montyclt@outlook.com>
2019-12-09 18:56:29 +0100

    Moved project to RCL

---
+++ MontyClt.Bui/wwwroot/bui.js
@@ -0,0 +1,12 @@
+﻿window.Bui =
+{
+    popper: function (activadorQuerySelector, boxQuerySelector, placement) {
+        new Popper(document.querySelector(activadorQuerySelector), document.querySelector(boxQuerySelector), { placement: placement });
+    },
+    datatable:
+    {
+        checkSelectAll: function (guid) {
+            return document.querySelector("#datatable-" + guid + " input[data-selectall]").checked;
+        }
+    }
+};
